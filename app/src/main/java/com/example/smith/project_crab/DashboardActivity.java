b8570651d9fa5c6eee8989fb398bd64bd4966b3e
smithshelke <smithshelke@gmail.com>
2018-12-15 21:36:50 +0530

    Fix UI bugs

--- app/src/main/java/com/example/smith/project_crab/DashboardActivity.java
+++ app/src/main/java/com/example/smith/project_crab/DashboardActivity.java
@@ -10,6 +10,7 @@ import android.os.Handler;
 import android.text.InputType;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -21,7 +22,8 @@ import java.util.Date;
 import java.util.Locale;
 
 public class DashboardActivity extends AppCompatActivity {
-    Calendar myCalendar;
+    private Calendar myCalendar;
+    private MyCustomSearchEditText customSearchEditText;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -29,13 +31,14 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         Toolbar appbar = findViewById(R.id.appbar);
-        FloatingActionButton fab  = findViewById(R.id.fab);
+        //FloatingActionButton fab  = findViewById(R.id.fab);
         EditText date = findViewById(R.id.date);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+        customSearchEditText = findViewById(R.id.customSearch);
         date.setInputType(InputType.TYPE_NULL);
         myCalendar = Calendar.getInstance();
 
         DatePickerDialog.OnDateSetListener datePicker = new DatePickerDialog.OnDateSetListener() {
-
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
@@ -61,6 +64,7 @@ public class DashboardActivity extends AppCompatActivity {
 
         // appbar.setLogo(R.drawable.googleg_disabled_color_18);
     }
+
     private String updateLabel() {
         String myFormat = "dd MMMM yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
--- app/src/main/java/com/example/smith/project_crab/MyCustomSearchEditText.java
+++ app/src/main/java/com/example/smith/project_crab/MyCustomSearchEditText.java
@@ -1,22 +1,27 @@
 package com.example.smith.project_crab;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.text.style.CharacterStyle;
-import android.transition.TransitionManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.AutocompletePrediction;
 import com.google.android.gms.location.places.AutocompletePredictionBufferResponse;
 import com.google.android.gms.location.places.GeoDataClient;
@@ -32,19 +37,35 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.transition.ChangeBounds;
+import androidx.transition.ChangeTransform;
+import androidx.transition.Fade;
+import androidx.transition.Slide;
+import androidx.transition.Transition;
+import androidx.transition.TransitionManager;
+import androidx.transition.TransitionSet;
 
 
 // The entry points to the Places API.
 public class MyCustomSearchEditText extends FrameLayout {
-    FrameLayout thisLayout = this;
-    EditText editText;
-    FrameLayout fl;
-    ViewGroup.LayoutParams layoutParamss;
+
+    public static final int EXPANDED_HEIGHT = 350;
+
+    private FrameLayout thisLayout = this;
+    private EditText editText;
+    private FrameLayout fl;
+    private boolean isExpanded = false;
+    private ViewGroup.LayoutParams layoutParamss;
+    private List<String> list = new ArrayList<>();
+    private Task<AutocompletePredictionBufferResponse> results;
     private static final LatLngBounds BOUNDS_INDIA = new LatLngBounds(new LatLng(23.63936, 68.14712), new LatLng(28.20453, 97.34466));
-
-
     private GeoDataClient mGeoDataClient;
     private PlaceDetectionClient mPlaceDetectionClient;
+    private ListView simpleList;
+
+    public String getText() {
+        return editText.getText().toString();
+    }
 
 
     public MyCustomSearchEditText(Context context) {
@@ -67,38 +88,72 @@ public class MyCustomSearchEditText extends FrameLayout {
         init(context);
     }
 
-    private void hide(){
-        TransitionManager.beginDelayedTransition(thisLayout);
-        layoutParamss.height= 0;
+    private void hide() {
+        Transition transition = new TransitionSet()
+                .addTransition(new ChangeBounds()
+                        .setDuration(250))
+                .setInterpolator(new AccelerateDecelerateInterpolator());
+        TransitionManager.beginDelayedTransition(thisLayout, transition);
+        layoutParamss.height = 0;
         fl.setLayoutParams(layoutParamss);
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    public void init(Context context){
-        inflate(context,R.layout.custom_edittext,this);
+    private void hideUsingValueAnimator() {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(EXPANDED_HEIGHT, 0);
+        valueAnimator.setInterpolator(new DecelerateInterpolator(.8f));
+        valueAnimator.setDuration(200);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int val = (int) animation.getAnimatedValue();
+                Log.d("Animation", "onAnimationUpdate: ");
+                //We retrieve the layout parameters and pick up the height of the View.
+                layoutParamss.height = val;
+                //Once we have updated the height all we need to do is to call the set method.
+                fl.setLayoutParams(layoutParamss);
+            }
+        });
+        valueAnimator.start();
+        isExpanded = false;
+    }
+
+    private void init(Context context) {
+        inflate(context, R.layout.custom_edittext, this);
         editText = findViewById(R.id.edit_text);
+        simpleList = (ListView) findViewById(R.id.list);
         fl = findViewById(R.id.frame);
         layoutParamss = fl.getLayoutParams();
         mGeoDataClient = Places.getGeoDataClient(context);
+        initEditText(context);
+    }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private void initEditText(Context context) {
+        editText.setClickable(false);
         editText.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction()==MotionEvent.ACTION_DOWN)
-                Log.d("FrameLayout", "onClick: "+layoutParamss.height);
-                TransitionManager.beginDelayedTransition(thisLayout);
-                layoutParamss.height= 300;
-                fl.setLayoutParams(layoutParamss);
+
+                if (event.getAction() == MotionEvent.ACTION_UP && layoutParamss.height == 0) {
+                    isExpanded = true;
+                    TransitionManager.beginDelayedTransition(thisLayout);
+                    layoutParamss.height = EXPANDED_HEIGHT;
+                    fl.setLayoutParams(layoutParamss);
+                    //return true;
+                }
+                if (event.getRawX() >= (editText.getRight() - editText.getCompoundDrawables()[2].getBounds().width())) {
+                    editText.setText("");
+                    return true;
+                }
                 return false;
             }
         });
         editText.setOnFocusChangeListener(new OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    TransitionManager.beginDelayedTransition(thisLayout);
-                    layoutParamss.height= 0;
-                    fl.setLayoutParams(layoutParamss);
+                Log.d("isExpanded", "val: " + isExpanded + hasFocus);
+                if (!hasFocus && isExpanded) {
+                    hideUsingValueAnimator();
                 }
             }
         });
@@ -115,28 +170,27 @@ public class MyCustomSearchEditText extends FrameLayout {
 
             @Override
             public void afterTextChanged(Editable s) {
-               String searchPlace = s.toString();
-                Task<AutocompletePredictionBufferResponse> results =
-                        mGeoDataClient.getAutocompletePredictions(searchPlace, BOUNDS_INDIA,null);
+                String searchPlace = s.toString();
+                results = mGeoDataClient.getAutocompletePredictions(searchPlace, BOUNDS_INDIA, null);
                 results.addOnCompleteListener(new OnCompleteListener<AutocompletePredictionBufferResponse>() {
                     @Override
                     public void onComplete(@NonNull Task<AutocompletePredictionBufferResponse> task) {
-                        if(task.getResult()!=null)
-                            Log.d("Key", "onComplete:###################### ");
-                        ListView simpleList = (ListView)findViewById(R.id.list);
-
-                        List<String> list = new ArrayList<>();
-                        for(AutocompletePrediction place : task.getResult()){
-                            Log.d("Result", "onComplete: "+place.getPrimaryText(null));
+                        AutocompletePredictionBufferResponse dataBuffer = task.getResult();
+                        if (task.getResult() != null)
+                            list.clear();
+                        Log.d("Key", "onComplete:###################### ");
+                        for (AutocompletePrediction place : task.getResult()) {
+                            Log.d("Result", "onComplete: " + place.getPrimaryText(null));
                             list.add(place.getFullText(null).toString());
                         }
-                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context,android.R.layout.simple_list_item_1,list);
+                        dataBuffer.release();
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, list);
                         simpleList.setDivider(null);
                         simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                             @Override
                             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                                 editText.setText(list.get(position));
-                                hide();
+                                hideUsingValueAnimator();
                             }
                         });
                         simpleList.setAdapter(arrayAdapter);
@@ -144,7 +198,6 @@ public class MyCustomSearchEditText extends FrameLayout {
                 });
             }
         });
-
     }
 
 }
